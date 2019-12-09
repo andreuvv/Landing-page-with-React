f62fe87fcc16422fefde452580969f052527ae1e
André <57376869+andreuvv@users.noreply.github.com>
2019-12-09 18:31:57 -0300

    nav y jumbo

---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+function NavBar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+            <a className="navbar-brand" href="#">Start Bootstrap</a>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Services</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default  NavBar
